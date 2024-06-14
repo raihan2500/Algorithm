4974d590329e48f391afa97c53bbfcbd31327308
Raihan Ahmed <raiihan2500@gmail.com>
2024-06-14 12:28:01 +0600

    Super Power Optimal

--- Number Theory/SuperPow.cpp
+++ Number Theory/SuperPow.cpp
@@ -3,51 +3,22 @@ using namespace std;
 #define print(arr)         for(auto x: arr)cout<<x<<' '; cout<<endl;
 #define int        long long
 
-
-
-int multiply(int a, int b, int m){
-    int ans = 0;
-
-    while(b > 0){
-        if(b & 1)ans = (ans + a) % m;
-        b >>= 1;
-        a = (a + a) % m;
-    }
-
-    return ans;
-}
-
-int power(int a, int b, int m){
-    int ans = 1;
-    while(b > 0){
-        if(b & 1) ans = multiply(ans, a, m);
-        b >>= 1;
-        a = multiply(a, a, m);
-    }
-    return ans;
-}
-
-int power2(int a, int b, int m){
-    int ans =  1;
-    while(b > 0){
-        if(b & 1)ans = (ans * a) %m;
-        b >>= 1;
-        a = (a * a) % m;
-    }
-    return ans;
-}
-
-
 int superPow(int a, vector<int>& b) {
+    int ans = 1;
 
-    int m = 1140;
-    int M = 1337;
-    int y = 0;
-    for(int i = b.size() - 1, j = 0; i >=0; i--, j++){
-        y = (y + (b[i] * power(10, j, m))) % m;
+    for(int i = b.size() - 1; i >= 0; --i){
+        for(int j = 0; j < b[i]; ++j){
+            ans *= a;
+            ans %= 1337;
+        }
+        int temp = a;
+        for(int j = 1; j < 10; ++j){
+            a *= temp;
+            a %= 1337;
+        }
     }
-    if(y == 0)y = 1140;
-    return power2(a, y, M);
+
+    return ans;    
 }
 
 
@@ -58,7 +29,4 @@ int32_t main(){
     for(int i = 0; i < n; i++) 
         cin>>b[i];
     cout<<superPow(a, b)<<endl;
-
-    cout<<power(7, 1140, 1337)<<endl;
-
 }
