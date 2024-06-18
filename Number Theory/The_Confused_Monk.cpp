2b047d991ca12951b76530b7e224468e3992f16d
Raihan Ahmed <raiihan2500@gmail.com>
2024-06-18 19:09:49 +0600

    binExp + Modulo

---
+++ Number Theory/The_Confused_Monk.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef                    long long ll;
+typedef                    long double ld;
+#define int                long long
+
+#define yes                cout<<"YES\n"
+#define no                 cout<<"NO\n"
+#define nl                 cout<<"\n"
+#define endl               "\n"
+
+#define lin(n)             ll n;cin>>n;
+#define in(n)              int n;cin>>n;
+#define vin                vector<int>
+#define pb(n)              push_back(n)
+#define pp                 pop_back()
+#define srt(v)             sort(v.begin(),v.end());
+#define all(x)             x.begin(),x.end()
+
+#define fi                 first
+#define se                 second
+#define mmp                make_pair
+
+#define sz(x)              ((int)(x).size())
+#define forn(i,e)          for(int i=0;i<e;i++)
+#define Forn(i,e)          for(int i=1;i<=e;i++)
+#define rforn(i,s)         for(int i=s-1;i>=0;i--)
+#define print(arr)         for(auto x: arr)cout<<x<<" ";nl;
+
+#define fast_in_out        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+const int M = 1e9 + 7;
+
+int binexp(int a, int b){
+    int ans = 1;
+    while(b > 0){
+        if(b & 1)ans = (ans * a) % M;
+        b >>= 1;
+        a = (a * a) % M;
+    }
+    return ans;
+}
+
+
+int32_t main()
+{
+    fast_in_out;
+    lin(n);
+    lin(x);
+    int a = x, b = x;
+    forn(i, n - 1){
+        lin(x);
+        a = __gcd(a, x);
+        b = (b * x) % M;
+    }
+    
+    cout<<binexp(b, a)<<endl;
+
+}
