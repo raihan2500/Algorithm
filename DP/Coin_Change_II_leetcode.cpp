92cffb310ab4409f30d5d84dbba3ba11692a5530
Raihan Ahmed <raiihan2500@gmail.com>
2024-06-25 12:54:32 +0600

    Accepted iterative dp

--- DP/Coin_Change_II_leetcode.cpp
+++ DP/Coin_Change_II_leetcode.cpp
@@ -2,31 +2,11 @@
 using namespace std;
 #define print(arr)         for(auto x: arr)cout<<x<<' '; cout<<endl;
 #define int        long long
-const int M = 1e9 + 7;
 
-int dp[5001][301];
-
-int recur(vector<int> &v, int x, int n){
-
-    int ans = 0;
-    if(x < 0)return 0;
-    if(x == 0)return 1;
-
-    for(int i = 0; i <= n; i++){
-        if(i > x)continue;
-        if(dp[x - v[i]][i] == -1){
-            dp[x - v[i]][i] = recur(v, x- v[i], i);
-        }
-
-        ans += dp[x - v[i]][i];
-    }
-
-    return ans;
-}
-
+int dp[5001];
 
 int change(int amount, vector<int>& coins) {
-    int dp[amount +  1] = {0};
+    
     dp[0] = 1;
     for(int i = 0; i < coins.size(); i++){
         for(int j = 0; j <= amount; j++){
