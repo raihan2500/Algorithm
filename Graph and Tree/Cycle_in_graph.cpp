2d92c8c0bc254d9de6446fb36cf07c5dc025f025
Raihan Ahmed <raiihan2500@gmail.com>
2024-08-20 23:43:06 +0600

    DFS is Cycle exists or not

--- Graph and Tree/Cycle_in_graph.cpp
+++ Graph and Tree/Cycle_in_graph.cpp
@@ -1,49 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define print(arr)         for(auto x: arr)cout<<x<<' '; cout<<endl;
-#define int        long long
-const int M = 1e9 + 7;
+
 const int N = 1e5;
-
 vector<int> graph[N];
-vector<bool> vis(N);
-
+bool vis[N];
+bool isLoopExists = false;
 
 bool dfs(int vertex, int par){
+    if(isLoopExists)return true;
+    
     vis[vertex] = true;
-    bool isLoopExists = false;
-    for(auto child : graph[vertex]){
-        if(vis[child] && child == par)continue;
-        if(vis[child])return true;
 
-        isLoopExists |= (child, vertex);
+    for(auto child : graph[vertex]){
+        if(child == par)continue;
+        if(vis[child])return true;
+        isLoopExists = dfs(child, vertex);
     }
 
     return isLoopExists;
+
 }
 
 
-int32_t main(){
+int main(){
     int v, e;
-    cin >> v >> e;
-
+    cin >> e >> v;
     for(int i = 0; i < e; i++){
         int v1, v2;
         cin >> v1 >> v2;
-
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
 
-    bool isLoopExists = false;
-
     for(int i = 1; i <= v; i++){
-        cout<<i<<" ";
-        cout<<dfs(i, 0)<<endl;
+        isLoopExists = false;
         if(vis[i])continue;
         if(dfs(i, 0)){
-            isLoopExists = true;
-            // break;
+            break;
         }
     }
 
