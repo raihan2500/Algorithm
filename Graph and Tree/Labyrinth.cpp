5b6f1501baa2434ecfeeb4717257185909b24fed
Raihan Ahmed <raiihan2500@gmail.com>
2024-09-02 12:37:02 +0600

    Solved with BFS

--- Graph and Tree/Labyrinth.cpp
+++ Graph and Tree/Labyrinth.cpp
@@ -7,23 +7,19 @@ typedef                    long double ld;
 
 #define yes                cout<<"YES\n"
 #define no                 cout<<"NO\n"
-#define nl                 cout<<"\n"
+#define nl                 cout<<"\n";
 #define endl               "\n"
 
-#define lin(n)             ll n;cin>>n;
-#define in(n)              int n;cin>>n;
+#define lin(n)             int n;cin>>n;
 #define vin                vector<int>
 #define pr                 pair<int, int>
 #define pb(n)              push_back(n)
 #define pp                 pop_back()
-#define srt(v)             sort(v.begin(),v.end());
 #define all(x)             x.begin(),x.end()
 
 #define fi                 first
 #define se                 second
-#define mmp                make_pair
 
-#define sz(x)              ((int)(x).size())
 #define forn(i,e)          for(int i=0;i<e;i++)
 #define Forn(i,e)          for(int i=1;i<=e;i++)
 #define rforn(i,s)         for(int i=s-1;i>=0;i--)
@@ -34,87 +30,119 @@ typedef                    long double ld;
 const long long INF = 1e18;
 const int M = 1e9 + 7;
 const int N = 1e3 + 100;
+
+vector<string> graph;
+vector<vector<char>>val(N, vector<char> (N));
+vector<vector<pr>> par(N, vector<pr> (N));
+
+bool vis[N][N], flg = 0;
 int n, m;
-vector<string> v;
-int vis[N][N];
-int cnt = 0;
-string big_string;
-
-string recur(int i, int j){
-    cnt++;
-    // if(cnt > 10)return "*";
-
-    if(i >= n || i < 0)return "*";
-    if(j >= m || j < 0)return "*";
-    if(v[i][j] == 'B')return "";
-    if(v[i][j] == '#')return "*";
-
-    v[i][j] = '#';
-    vector<string> ans(4);
 
 
-    ans[0] ="D" + recur(i + 1, j);
-    ans[1] ="U" + recur(i - 1, j);
+vector<pr> movements = {
+    {1, 0},
+    {-1, 0}, 
+    {0, 1},
+    {0, -1}
+};
 
-    ans[2] ="R" + recur(i, j + 1);
-    ans[3] ="L" + recur(i, j - 1);
+char indentify(int x, int y){
+    if(x == 0){
+        if(y == 1)return 'R';
+        else return 'L';
+    }
+    if(x == 1)return 'D';
+    if(x == -1)return 'U';
+    return 'X';
+}
 
+bool isValid(int x, int y){
+    return x >= 0 && x < n && y >=0 && y < m;
+}
 
+void bfs(int x, int y){
+    queue<pr> q;
+    q.push({x, y});
+    vis[x][y] = true;
 
-    string a = big_string;
-    int ln = INF;
-    
-    for(auto it : ans){
-        // bool ck = 0;
-        // for(auto c : it)if(c == '*'){ck = 1; break;}
-        // if(ck)continue;
-        if(it.back() == '*')continue;
-        if(it.size() < ln){
-            ln = it.size();
-            a = it;
+    while(!q.empty()){
+        int vx = q.front().first;
+        int vy = q.front().second;
+        q.pop();
+
+        for(auto movement : movements){
+            int childx = vx + movement.first;
+            int childy = vy + movement.second;
+
+            if(!isValid(childx, childy))continue;
+            if(graph[childx][childy] == '#')continue;
+            if(vis[childx][childy])continue;
+            vis[childx][childy] = true;
+
+            par[childx][childy] = {vx, vy};
+            val[childx][childy] = indentify(movement.first, movement.second);
+            q.push({childx, childy});
+            if(graph[childx][childy] == 'B'){
+                flg = true;
+                return;
+            }
         }
     }
-    return a;
 }
 
 
-int32_t main()
-{
+int32_t main(){
     fast_in_out;
-    for(int i = 0; i < n * m; i++)big_string.push_back('Z');
-
-
     cin >> n >> m;
-    forn(i,n){
+    for(int i = 0; i < n; i++){
         string str;
         cin >> str;
-        v.pb(str);
+        graph.push_back(str);
     }
-    int i = 0, j = 0;
-    for(; i < n; i++){
+    int x = -1, y = -1;
+
+    for(int i = 0; i < n; i++){
         bool ck = 0;
-        for(j = 0; j < m; j++){
-            if(v[i][j] == 'A'){
+        for(int j = 0; j < m; j++){
+            if(graph[i][j] == 'A'){
+                x = i;
+                y = j;
                 ck = 1;
                 break;
             }
         }
         if(ck)break;
     }
-    if(v[i][j] != 'A'){
+    bfs(x, y);
+    for(int i = 0; i < n; i++){
+        bool ck = 0;
+        for(int j = 0; j < m; j++){
+            if(graph[i][j] == 'B'){
+                x = i;
+                y = j;
+                ck = 1;
+                break;
+            }
+        }
+        if(ck)break;
+    }
+
+    if(!flg){
         no;
         return 0;
     }
 
-    string ans = recur(i, j);
+    string ans;
+    ans.push_back(val[x][y]);
 
-    if(ans == big_string || ans == "*"){
-        no;
-        return 0;
+    for(;graph[x][y] != 'A';){
+        pr p = par[x][y];
+        x = p.first; y = p.second;
+        ans.push_back(val[x][y]);
     }
-
+    ans.pop_back();
+    reverse(all(ans));
     yes;
-    cout<<ans.size()<<endl;
+    cout<<ans.size()<<endl;    
     cout<<ans<<endl;
-    
 }
