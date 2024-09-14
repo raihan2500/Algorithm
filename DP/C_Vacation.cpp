1a63f9b356d33dfcabd8e900b07f80bcea848667
Raihan Ahmed <raiihan2500@gmail.com>
2024-09-15 01:22:04 +0600

    reunion

--- DP/C_Vacation.cpp
+++ DP/C_Vacation.cpp
@@ -1,80 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef                    long long ll;
-typedef                    long double ld;
-#define int                long long
+const int N = 1e5 + 100;
 
-#define yes                cout<<"YES\n"
-#define no                 cout<<"NO\n"
-#define nl                 cout<<"\n"
-#define endl               "\n"
+vector<vector<int>> v;
+int n, dp[N][3];
 
-#define lin(n)             ll n;cin>>n;
-#define in(n)              int n;cin>>n;
-#define vin                vector<int>
-#define pb(n)              push_back(n)
-#define pp                 pop_back()
-#define srt(v)             sort(v.begin(),v.end());
-#define all(x)             x.begin(),x.end()
+int recur(int x, int y){
 
-#define fi                 first
-#define se                 second
-#define mmp                make_pair
-
-#define sz(x)              ((int)(x).size())
-#define forn(i,e)          for(int i=0;i<e;i++)
-#define Forn(i,e)          for(int i=1;i<=e;i++)
-#define rforn(i,s)         for(int i=s-1;i>=0;i--)
-#define print(arr)         for(auto x: arr)cout<<x<<" ";nl;
-
-#define fast_in_out        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-const int M = 1e9 + 7;
-const int N = 1e5 + 10;
-vector<vector<int>> v, dp(N, vin (3)) ;
-
-
-
-int func(int n, int j){
-    int mx = 0;
+    if(x == n)return 0;
+    if(dp[x][y] != 0)return dp[x][y];
     
-    if(n < 0)return 0;
-    if(n == 0)return v[n][j];
-
-    for(int i = 0; i < 3; i++){
-        if(i == j)continue;
-
-        if(dp[n - 1][i] == -1){
-            dp[n - 1][i] == func(n - 1, i);
-        }
-        mx = max(mx, v[n][j] + dp[n - 1][i]);
-    }
-    return dp[n][j] = mx;
-
-}
-
-int32_t main()
-{
-    fast_in_out;
-    lin(n);
-
-    for(auto &a : dp)
-        for(auto &x : a)x = - 1;
-
-    forn(i,n){
-        vin vv(3);
-        forn(j, 3)cin>>vv[j];
-        v.push_back(vv);
-    }
-
-    for(int i = 0; i < 3; i++)
-        dp[0][i] = v[0][i];
-
-
     int ans = 0;
     for(int i = 0; i < 3; i++){
-        ans = max(ans, func(n - 1, i));
+        if(i == y)continue;
+        int temp = v[x][i] + recur(x + 1, i);
+        if(temp > ans){
+            ans = temp;
+        }
     }
-    cout<<ans<<endl;    
-    
+
+    return dp[x][y] = ans;
+}
+
+int32_t main(){
+    cin >> n;
+    for(int i = 0;i  <n; i++){
+        vector<int> temp(3);
+        for(int j = 0; j < 3; j++)cin >> temp[j];
+        v.push_back(temp);
+    }   
+
+    int ans = recur(0, -1); 
+    cout << ans << endl;
+
 }
