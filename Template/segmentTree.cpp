49f066f0793073e87f069a5e2c764d1e3acb6c79
Raihan Ahmed <raiihan2500@gmail.com>
2024-09-19 02:38:02 +0600

    Segment Tree updated

--- Template/segmentTree.cpp
+++ Template/segmentTree.cpp
@@ -2,59 +2,59 @@
 using namespace std;
 
 
+template<class Type>
 class segTree{
     #define int long long
     private:
-        int n, q;
-        string type;
         const int N = 1e6 + 10;
-        vector<int> v = vector<int> (N), tree = vector<int> (4 * N);
+        int n;
+        vector<int> v;
+        vector<Type> tree = vector<Type> (4 * N);
+
     public:
-        segTree(string type, vector<int> v){
+        segTree(vector<int> v){
             this->v = v;
             n = v.size();
-            this->type = type;
             build(1, 0, n - 1);
         }
-        void Update(int l, int r){
-            update(1, 0, n - 1, l, r);
+
+        void Update(int pos, int val){
+            update(1, 0, n - 1, pos, val);
         }
-        int Query(int l, int r){
+        Type Query(int l, int r){
             return query(1, 0, n - 1, l, r);
         }
 
     private:
-        int query(int node, int lo, int hi, int l, int r){
+        Type query(int node, int lo, int hi, int l, int r){
+
             if(hi < l or lo > r){
-                if(type == "sum")return 0;
-                if(type == "min")return 1e18;
-                if(type == "max")return -1e18;
+                //Edit here
+                return 0;
             }
             if(lo >= l and hi <= r)return tree[node];
 
             int mid = (lo + hi) / 2;
-            int q1 = query(2 * node, lo, mid, l, r);
-            int q2 = query(2 * node + 1, mid + 1, hi, l, r);
+            Type q1 = query(2 * node, lo, mid, l, r);
+            Type q2 = query(2 * node + 1, mid + 1, hi, l, r);
 
-            if(type == "sum")return q1 + q2;
-            if(type == "min")return min(q1, q2);
-            if(type == "max")return max(q1, q2);
+            //Edit here
+            return q1 + q2;
         }
 
-        void update(int node, int lo, int hi, int l, int r){
-            if(hi < l or lo > l)return;
+        void update(int node, int lo, int hi, int pos, int val){
+            if(hi < pos or lo > pos)return;
             if(hi == lo){
-                tree[node] = r;
+                tree[node] = val;
                 return;
             }
 
             int mid = (lo + hi) / 2;
-            update(2 * node, lo, mid, l, r);
-            update(2 * node + 1, mid + 1, hi, l, r);
+            update(2 * node, lo, mid, pos, val);
+            update(2 * node + 1, mid + 1, hi, pos, val);
 
-            if(type == "sum")tree[node] = tree[2 * node] + tree[2 * node + 1];
-            if(type == "min")tree[node] = min(tree[2 * node], tree[2 * node + 1]);
-            if(type == "max")tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+            //Edit here
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
         }
 
         void build(int node, int lo, int hi){
@@ -66,11 +66,12 @@ class segTree{
             int mid = (lo + hi) / 2;
             build(2 * node, lo, mid);
             build(2 * node + 1, mid + 1, hi);
-            if(type == "sum")tree[node] = tree[2 * node] + tree[2 * node + 1];
-            if(type == "min")tree[node] = min(tree[2 * node], tree[2 * node + 1]);
-            if(type == "max")tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+
+            //Edit here
+            tree[node] = tree[2 * node] + tree[2 * node + 1];
         }
 };
+
 
 int32_t main(){
     int n, q;
@@ -79,7 +80,7 @@ int32_t main(){
     for(int i = 0; i < n; i++){
         cin >> v[i];
     }
-    segTree seg("min", v);
+    segTree<int> seg(v);
 
     while(q--){
         int type, l, r;
